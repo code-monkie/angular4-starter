762d9159354b6672231ea4670001a177f4ce581c
Mike Dunn <mikeykhan2003@gmail.com>
2017-06-29 00:49:10 -0500

    Removed unused imports from app.module

--- angular4-starter/src/app/app.module.ts
+++ angular4-starter/src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { SigninComponent } from './auth/signin/signin.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthService } from './auth/auth.service';
 import { ConstantsService } from './constants.service';
