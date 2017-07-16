865f099961d482ecb1c76a68c81d11cf50e4195e
Mike Dunn <mikeykhan2003@gmail.com>
2017-07-16 04:44:39 -0500

    Removed unused auth

--- angular4-starter/src/app/auth/auth.module.ts
+++ angular4-starter/src/app/auth/auth.module.ts
@@ -5,13 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { RegisterComponent } from './register/register.component';
-import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
     declarations: [
         SigninComponent,
         RegisterComponent,
-        AuthComponent,
     ],
     imports: [
         CommonModule,
